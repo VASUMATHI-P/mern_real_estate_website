8ccebb8bfe60659b66b7e58b27874abe5189652b
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2024-08-16 16:29:53 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO)
+        .then(() => {
+          console.log('Connected to MongoDB');
+        }).catch((err) => {
+          console.log('Error connecting to MongoDB:', err);
+        })
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!`);
