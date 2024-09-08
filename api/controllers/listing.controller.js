d430f9004b5d8f510460b259d8dca28898af428c
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2024-09-08 12:33:01 +0530

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -59,3 +59,47 @@ export const getListing = async (req, res, next) => {
     next(err);
   }
 }
+
+export const getListings = async (req, res, next) => {
+  const limit = parseInt(req.query.limit) || 10;
+  const startIndex = parseInt(req.query.startIndex) || 0;
+  const sort = req.query.sort || 'createdAt';
+  const order = req.query.order || 'desc';
+  const searchTerm = req.query.searchTerm || '';
+
+  let offer = req.query.offer;
+  if(offer === undefined || offer === 'false'){
+    offer = { $in : [true, false]}
+  }
+
+  let furnished = req.query.furnished;
+  if(furnished === undefined || furnished === 'false'){
+    furnished = { $in : [true, false] }
+  }
+
+  let parking = req.query.parking;
+  if(parking === undefined || parking === 'false'){
+    parking = { $in : [true, false] }
+  }
+
+  let type = req.query.type;
+  if(type === undefined || type === 'all'){
+    type = { $in : ['rent', 'sale'] }
+  }
+
+  try {
+    const listings = await Listing.find({
+      name : {$regex: searchTerm, $options: 'i'},
+      offer,
+      parking,
+      furnished,
+      type
+    }).sort({[sort] : order})
+      .limit(limit)
+      .skip(startIndex)
+
+    res.status(200).json(listings);
+  } catch(err){
+    next(err);
+  }
+}
